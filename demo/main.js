69c3aaa62b1f44b030b822e5efd167482ffb4f3c
Adrian Toncean <adrianton3@gmail.com>
2017-06-10 09:25:46 +0200

    Add a way to measure compile/execution time

--- demo/main.js
+++ demo/main.js
@@ -10,7 +10,9 @@
 	codeElement.value = '++++++++[>++++++++<-]>+.'
 
 	function runWasm (source, input) {
+		console.time('bf->wasm compile')
 		const bytes = bfToWasm(source)
+		console.timeEnd('bf->wasm compile')
 
 		const output = []
 		let inputPointer = 0
@@ -30,14 +32,18 @@
 
 		return WebAssembly.instantiate(new Uint8Array(bytes), importObject)
 			.then(({ instance }) => {
+				console.time('wasm run')
 				instance.exports.run()
+				console.timeEnd('wasm run')
 
 				return output
 			})
 	}
 
 	function runJs (source, input) {
+		console.time('bf->js compile')
 		const run = bjs.bfToJs(source)
+		console.timeEnd('bf->js compile')
 
 		const output = []
 		let inputPointer = 0
@@ -52,7 +58,9 @@
 			output.push(value)
 		}
 
+		console.time('js run')
 		run({ read, write })
+		console.timeEnd('js run')
 
 		return Promise.resolve(output)
 	}
@@ -61,12 +69,14 @@
 		return () => {
 			outputElement.value = ''
 
-			run(
-				codeElement.value,
-				inputElement.value.split(/\s+/).map(Number)
-			).then((output) => {
-				outputElement.value = String.fromCharCode(...output)
-			})
+			setTimeout(() => {
+				run(
+					codeElement.value,
+					inputElement.value.split(/\s+/).map(Number)
+				).then((output) => {
+					outputElement.value = String.fromCharCode(...output)
+				})
+			}, 4)
 		}
 	}
 
